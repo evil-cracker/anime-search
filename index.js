9f0b185c87d8a1521e28690624ee5fbf6c981efe
Evil <vinothdhanu90@gmail.com>
2023-07-30 13:19:27 +0530

    "initial-setup-done"

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+var app=express();
+var port=3000;
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running in ${port} port location `);
+})
