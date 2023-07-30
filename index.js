1f20e3f6f5953dfba73c463746f05232fab53580
Evil <vinothdhanu90@gmail.com>
2023-07-30 16:06:33 +0530

    "java-script-work-complete"

--- index.js
+++ index.js
@@ -4,15 +4,45 @@ import axios from "axios";
 
 var app=express();
 var port=3000;
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+var myurl="https://kitsu.io/api/edge/anime?filter[text]="
+
+
+
+
 
 
 app.get("/",(req,res)=>{
-
     res.render("index.ejs");
+   
 });
 
+
+
+
+app.post("/submit", async(req,res)=>{
+    
+   var name= req.body.ani;
+   name=name.toUpperCase();
+
+   const details = await axios.get(myurl+name);
+
+   var gen= details.data.data[0].relationships.genres.links.related;
+   const gener = await axios.get(gen);
+
+
+
+
+   
+    
+    res.render("detail.ejs",{
+    
+     name:name,
+     data:details.data.data,
+     gener:gener.data.data,
+    });
+});
 
 
 
@@ -20,4 +50,4 @@ app.get("/",(req,res)=>{
 
 app.listen(port,()=>{
     console.log(`Server is running in ${port} port location click here to redirect http://localhost:3000/ `);
-})
+});
